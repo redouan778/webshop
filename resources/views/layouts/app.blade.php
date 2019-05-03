bc468ad7a4e50178b1973075c71ca8c9963b417a
redouan <redouan778@gmail.com>
2019-05-03 03:10:19 +0200

    checkout page almost done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,12 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/products.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/webshop.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/webshop.js') }}" ></script>
 
 </head>
 <body>
--- resources/views/shoppingCart.blade.php
+++ resources/views/shoppingCart.blade.php
@@ -5,21 +5,50 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
           @if(count($orders) === 0)
-            winkelkar is leeg
-          
+            <h3>Shopping Cart</h3>
+            <p>You Have not selected any product yet.</p>
           @endif
-            @foreach ($orders as $order)
-            <div class="card" style="width: 18rem;" >
-              <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">{{$order->name}}</h5>
-                  <p class="card-text">{{$order->description}}</p>
-                  <p class="card-text">€ {{$order->price}}</p>
-                  <a href="{{ url('#') }}" class="btn btn-primary">Add Product to Cart</a>
-                </div>
-              </div>
-              @endforeach
+          <a href="{{ url('homepage') }}" class="btn btn-primary">< Go back to the products</a>
+
       </div>
     </div>
+</div>
+
+<div class="shopping-cart">
+  <!-- Title -->
+  <div class="title">
+    Shopping Bag
+  </div>
+  @foreach ($orders as $order)
+
+  <!-- Product #1 -->
+  <div class="item">
+    <div class="buttons">
+      <span class="delete-btn"></span>
+      <span class="like-btn"></span>
+    </div>
+
+    <div class="image">
+      <img src="item-1.png" alt="sku" />
+    </div>
+
+    <div class="description">
+      <span>{{$order->name}}</span>
+
+    </div>
+
+    <div class="quantity">
+      <button class="plus-btn" type="button" name="button">
+        <img src="plus.svg" alt="" />
+      </button>
+      <input type="text" name="name" value="1">
+      <button class="minus-btn" type="button" name="button">
+        <img src="minus.svg" alt="" />
+      </button>
+    </div>
+
+    <div class="total-price">€{{$order->price}}</div>
+  </div>
+  @endforeach
 </div>
 @endsection
