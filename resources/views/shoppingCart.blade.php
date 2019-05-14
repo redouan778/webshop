fed48e0d7f4b78c3d072eb92caee9d437f2cc5b9
redouan <redouan778@gmail.com>
2019-05-14 22:38:30 +0200

    ShoppingCart function is done

--- resources/views/shoppingCart.blade.php
+++ resources/views/shoppingCart.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-md-12">s
           @if(Session::has('cart'))
 
           @foreach($products as $product)
